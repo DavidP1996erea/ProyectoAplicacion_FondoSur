0a43db65b12a6ddf71ed6aeff8380316c5cca60b
DavidP1996erea <david.perea@iesnervion.es>
2023-01-27 13:14:33 +0100

    Agregar archivos de proyecto.

---
+++ PregunFondoSur/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PregunFondoSur
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SignalR_Proyecto/Program.cs
@@ -0,0 +1,27 @@
+using SignalR_Proyecto.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages(); 
+
+
+app.MapHub<preguntadosHub>("/preguntadosHub");
+
+app.Run();
