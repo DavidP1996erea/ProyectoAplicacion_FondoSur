4dea250bf4e05947701f3214d8e7c98f718d79f2
AlejandroMulero1 <alejandro.mulero@iesnervion.es>
2023-02-15 14:38:17 +0100

    v55

--- PregunFondoSur/ViewModels/clsPreguntasVM.cs
+++ PregunFondoSur/ViewModels/clsPreguntasVM.cs
@@ -1,13 +1,36 @@
-﻿using System;
+﻿using Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using PregunFondoSur.ViewModels.Utilidades;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PregunFondoSur.ViewModels.clsPreguntasVM
 {
-    public class clsPreguntasVM
+    public class clsPreguntasVM : clsVMBase
     {
+        #region Atributos
+        private clsPreguntas pregunta;
+        private int tiempo;
+        private DelegateCommand pulsarBotonCommand;
+        #endregion
 
+        #region Propiedades
+        public clsPreguntas Pregunta { get { return pregunta; } set { pregunta = value; } }
+        public int Tempo { get { return tiempo; } set { tiempo = value; } }
+        public DelegateCommand PulsarBotonCommand { get { return pulsarBotonCommand; } }
+        #endregion
+
+        #region Constructores
+        public clsPreguntasVM() {
+            tiempo = 60;
+            pulsarBotonCommand = new DelegateCommand();
+        }
+        #endregion
+
+        #region SignalR
+        #endregion
     }
 }
+
