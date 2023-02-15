33b659ce2fb8f7521131ae560dd589e6546467bc
DavidP1996erea <david.perea@iesnervion.es>
2023-02-15 13:42:29 +0100

    6

--- PregunFondoSur/ViewModels/clsPreguntasVM.cs
+++ PregunFondoSur/ViewModels/clsPreguntasVM.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PregunFondoSur.ViewModels
+namespace PregunFondoSur.ViewModels.clsPreguntasVM
 {
     internal class clsPreguntasVM
     {
---
+++ PregunFondoSur/Vistas/PaginaPregunta.xaml.cs
@@ -0,0 +1,9 @@
+namespace PregunFondoSur.Vistas;
+
+public partial class PaginaPregunta : ContentPage
+{
+	public PaginaPregunta()
+	{
+		InitializeComponent();
+	}
+}
