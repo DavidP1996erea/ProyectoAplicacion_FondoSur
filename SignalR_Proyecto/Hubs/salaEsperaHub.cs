b3e89e2e85c750b2a6ddb23fbad3ad745aecc23b
David Perea <david.perea@iesnervion.es>
2023-02-21 18:58:34 +0100

    post post muletas

--- SignalR_Proyecto/Hubs/salaEsperaHub.cs
+++ SignalR_Proyecto/Hubs/salaEsperaHub.cs
@@ -1,14 +1,15 @@
 ﻿using Microsoft.AspNetCore.SignalR;
-
+using SignalR_Proyecto.Models;
 
 namespace SignalR_Proyecto.Hubs
 {
     public class salaEsperaHub :Hub
     {
 
+        
         public async Task enviarBool(string hayRival)
         {
-
+            clsUsuariosPartida.ListadoUsuariosPartida.Clear();
             await Clients.Others.SendAsync("recibirBool", hayRival);
 
         }
