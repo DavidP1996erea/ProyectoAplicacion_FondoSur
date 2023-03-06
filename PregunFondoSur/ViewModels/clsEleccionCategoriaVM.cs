2365972ccb9f3f2a5b225fe9fcade2008c769c50
David Perea <93648772+DavidP1996erea@users.noreply.github.com>
2023-03-06 14:12:40 +0100

    v8y

--- PregunFondoSur/ViewModels/clsEleccionCategoriaVM.cs
+++ PregunFondoSur/ViewModels/clsEleccionCategoriaVM.cs
@@ -72,7 +72,7 @@ namespace PregunFondoSur.ViewModels
                 // Se comprueba que antes de realizar las acciones el usuario no esté a null
                 if (usuarioLocal != null)
                 {
-                    if (enviadoUsuario == 0)
+                    if (enviadoUsuario <=2)
                     {
                         enviadoUsuario++;
                         enviarUsuario();
